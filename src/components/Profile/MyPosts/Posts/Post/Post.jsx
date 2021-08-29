76e00b4bdc48637371d46c44d85c6f0f5ff5960c
Roman <romanblyashuk94@gmail.com>
2021-08-29 23:03:35 +0300

    16th lesson done. Adding props.

--- src/components/Profile/MyPosts/Posts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Posts/Post/Post.jsx
@@ -1,14 +1,18 @@
 import s from './Post.module.scss'
 
 
-const Post = () => {
+const Post = (props) => {
   return (
   <div className={s.post}>
     <div className={s.post__wrap}>
       <img src="https://www.ejin.ru/wp-content/uploads/2017/12/46861.jpg" alt="Ava" className={s.poster_ava} />
       <div className={s.post__body}>
-        <p className={s.post__date}>8/25/2021 5:21 PM</p>
-        <p className={s.post__text}> Sasuke said, that he don't want return to vilage! :((</p>
+        <p className={s.post__date}>{props.date}</p>
+        <p className={s.post__text}> {props.message} </p>
+        <div className={s.likes}>
+          <a href="S#"><img className={s.like_icon} src="https://www.vectorico.com/wp-content/uploads/2018/02/Like-Icon.png" alt="Like" /></a>
+          <span className={s.likes_count}>{props.likesCount} Likes</span>
+        </div>
       </div>
     </div>
   </div>
--- src/components/Profile/MyPosts/Posts/Posts.jsx
+++ src/components/Profile/MyPosts/Posts/Posts.jsx
@@ -5,9 +5,8 @@ import s from './Posts.module.scss'
 const Posts = () => {
   return (
     <div className={s.posts}>
-      <Post />
-      <Post />
-      <Post />
+      <Post likesCount="10" date="8/29/2021 8:36 PM" message="Sasuke said, that he don't want return to vilage! :((" />
+      <Post likesCount="8"date="8/28/2021 6:24 PM" message="Studied the Rasengan technique today. It turned out to be more difficult than I thought. I hope I succeed"/>
     </div>
   )
 }
